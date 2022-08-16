ad47e6655c8f4ddfc1153ced882106cd33b8e0f0
ujma1234 <ujma1234@naver.com>
2022-08-17 02:01:00 +0900

    game inro

---
+++ game.js
@@ -0,0 +1,260 @@
+const article1 = document.querySelector(".article1");
+const article2 = document.querySelector(".article2");
+const article3 = document.querySelector(".article3");
+const article4 = document.querySelector(".article4");
+const article5 = document.querySelector(".article5");
+const article6 = document.querySelector(".article6");
+const article7 = document.querySelector(".article7");
+const article8 = document.querySelector(".article8");
+const article9 = document.querySelector(".article9");
+const article10 = document.querySelector(".article10");
+const article11 = document.querySelector(".article11");
+const article12 = document.querySelector(".article12");
+const article13 = document.querySelector(".article13");
+const article14 = document.querySelector(".article14");
+const article15 = document.querySelector(".article15");
+const article16 = document.querySelector(".article16");
+const article17 = document.querySelector(".article17");
+const article18 = document.querySelector(".article18");
+const article19 = document.querySelector(".article19");
+const article20 = document.querySelector(".article20");
+const article21 = document.querySelector(".article21");
+const article22 = document.querySelector(".article22");
+const article23 = document.querySelector(".article23");
+const article24 = document.querySelector(".article24");
+const article25 = document.querySelector(".article25");
+const article26 = document.querySelector(".article26");
+const article27 = document.querySelector(".article27");
+const article28 = document.querySelector(".article28");
+const article29 = document.querySelector(".article29");
+const article30 = document.querySelector(".article30");
+
+var i = 1;
+article1.addEventListener("click", e=>{
+    if(i<5) {
+        article1.className = "select"+i.toString();
+        i++;
+    }
+});
+article2.addEventListener("click", e=>{
+    if(i<5) {
+        article2.className = "select"+i.toString();
+        i++;
+    }
+});
+article3.addEventListener("click", e=>{
+    if(i<5) {
+        article3.className = "select"+i.toString();
+        i++;
+    }
+});
+article4.addEventListener("click", e=>{
+    if(i<5) {
+        article4.className = "select"+i.toString();
+        i++;
+    }
+});
+article5.addEventListener("click", e=>{
+    if(i<5) {
+        article5.className = "select"+i.toString();
+        i++;
+    }
+});
+article6.addEventListener("click", e=>{
+    if(i<5) {
+        article6.className = "select"+i.toString();
+        i++;
+    }
+});
+article7.addEventListener("click", e=>{
+    if(i<5) {
+        article7.className = "select"+i.toString();
+        i++;
+    }
+});
+article8.addEventListener("click", e=>{
+    if(i<5) {
+        article8.className = "select"+i.toString();
+        i++;
+    }
+});
+article9.addEventListener("click", e=>{
+    if(i<5) {
+        article9.className = "select"+i.toString();
+        i++;
+    }
+});
+article10.addEventListener("click", e=>{
+    if(i<5) {
+        article10.className = "select"+i.toString();
+        i++;
+    }
+});
+article11.addEventListener("click", e=>{
+    if(i<5) {
+        article11.className = "select"+i.toString();
+        i++;
+    }
+});
+article12.addEventListener("click", e=>{
+    if(i<5) {
+        article12.className = "select"+i.toString();
+        i++;
+    }
+});
+article13.addEventListener("click", e=>{
+    if(i<5) {
+        article13.className = "select"+i.toString();
+        i++;
+    }
+});
+article14.addEventListener("click", e=>{
+    if(i<5) {
+        article14.className = "select"+i.toString();
+        i++;
+    }
+});
+article15.addEventListener("click", e=>{
+    if(i<5) {
+        article15.className = "select"+i.toString();
+        i++;
+    }
+});
+article16.addEventListener("click", e=>{
+    if(i<5) {
+        article16.className = "select"+i.toString();
+        i++;
+    }
+});
+article17.addEventListener("click", e=>{
+    if(i<5) {
+        article17.className = "select"+i.toString();
+        i++;
+    }
+});
+article18.addEventListener("click", e=>{
+    if(i<5) {
+        article18.className = "select"+i.toString();
+        i++;
+    }
+});
+article19.addEventListener("click", e=>{
+    if(i<5) {
+        article19.className = "select"+i.toString();
+        i++;
+    }
+});
+article20.addEventListener("click", e=>{
+    if(i<5) {
+        article20.className = "select"+i.toString();
+        i++;
+    }
+});
+article21.addEventListener("click", e=>{
+    if(i<5) {
+        article21.className = "select"+i.toString();
+        i++;
+    }
+});
+article22.addEventListener("click", e=>{
+    if(i<5) {
+        article22.className = "select"+i.toString();
+        i++;
+    }
+});
+article23.addEventListener("click", e=>{
+    if(i<5) {
+        article23.className = "select"+i.toString();
+        i++;
+    }
+});
+article24.addEventListener("click", e=>{
+    if(i<5) {
+        article24.className = "select"+i.toString();
+        i++;
+    }
+});
+article25.addEventListener("click", e=>{
+    if(i<5) {
+        article25.className = "select"+i.toString();
+        i++;
+    }
+});
+article26.addEventListener("click", e=>{
+    if(i<5) {
+        article26.className = "select"+i.toString();
+        i++;
+    }
+});
+article27.addEventListener("click", e=>{
+    if(i<5) {
+        article27.className = "select"+i.toString();
+        i++;
+    }
+});
+article28.addEventListener("click", e=>{
+    if(i<5) {
+        article28.className = "select"+i.toString();
+        i++;
+    }
+});
+article29.addEventListener("click", e=>{
+    if(i<5) {
+        article29.className = "select"+i.toString();
+        i++;
+    }
+});
+article30.addEventListener("click", e=>{
+    if(i<5) {
+        article30.className = "select"+i.toString();
+        i++;
+    }
+});
+
+const init = document.querySelector(".initial");
+init.addEventListener("click", e=>{
+    i=1;
+    article1.className = "article1";
+    article2.className = "article2";
+    article3.className = "article3";
+    article4.className = "article4";
+    article5.className = "article5";
+    article6.className = "article6";
+    article7.className = "article7";
+    article8.className = "article8";
+    article9.className = "article9";
+    article10.className = "article10";
+    article11.className = "article11";
+    article12.className = "article12";
+    article13.className = "article13";
+    article14.className = "article14";
+    article15.className = "article15";
+    article16.className = "article16";
+    article17.className = "article17";
+    article18.className = "article18";
+    article19.className = "article19";
+    article20.className = "article20";
+    article21.className = "article21";
+    article22.className = "article22";
+    article23.className = "article23";
+    article24.className = "article24";
+    article25.className = "article25";
+    article26.className = "article26";
+    article27.className = "article27";
+    article28.className = "article28";
+    article29.className = "article29";
+    article30.className = "article30";
+});
+
+const next = document.querySelector(".next");
+next.addEventListener("click", e=>{
+    if( i !=5 ) {
+        document.getElementById("wrn").className = "warning";
+        const timeout = setTimeout(()=>{
+            document.getElementById("wrn").className = "off_warning"
+        },2000)
+    }
+    else {
+        document.querySelector(".explain").className = "off_explain"
+    }
+});
