52ee399c13ac45f02a0f2d120b067c2cd093aedc
ujma1234 <ujma1234@naver.com>
2022-08-29 23:25:49 +0900

    finish fin

--- upload.js
+++ upload.js
@@ -30,3 +30,20 @@ btn.addEventListener("click", e=>{
         location.replace("taking_pic.html")
     }, 7000);
 })
+
+var myVideoStream = document.getElementById('myVideo')     // make it a global variable
+  var myStoredInterval = 0
+  
+function getVideo(){
+  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  navigator.getMedia({video: true, audio: false},
+                     
+    function(stream) {
+      myVideoStream.srcObject = stream   
+      myVideoStream.play();
+  }, 
+                     
+   function(error) {
+     alert('webcam not working');
+  });
+}
